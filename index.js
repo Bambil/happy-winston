b3eec4dc684e2b58d4a81c603fb8a2687e25ef0e
Parham Alvani <parham.alvani@gmail.com>
2017-08-24 17:44:40 +0430

    Adds module entry point.

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 24-08-2017
+ * |
+ * | File Name:     index.js
+ * +===============================================
+ */
+module.exports = {
+  Winstond: require('./lib/winstond')
+}
